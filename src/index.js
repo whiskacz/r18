5d513d50f90ef0847cf38682cbed0cfc33e3d085
whiskacz <pawelblasiak1985@gmail.com>
2023-07-21 23:15:32 +0200

    React Practice r18

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function R18App () {
+
+    const reference = useRef(null)
+
+    useEffect(() => {
+        reference.current.focus()
+    })
+
+    return (
+        <div style={{
+            position:"absolute",
+            top:"50%",
+            left:"50%",
+            transform:"translate(-50%,-50%)",
+            width:'50vw',
+            height:'50vh',
+            display:'flex',
+            justifyContent:'center',
+            alignItems:'center',
+            flexDirection:'column',
+            backgroundColor:"lightblue",
+            borderRadius:'10px'
+        }}>
+            <input type="text" style={{
+                border: 'none',
+                borderBottom:'2px solid black'
+            }}  placeholder='enter text to convert'
+                ref={reference}/>
+            <button style={{
+                margin:'50px',
+                width:'100px',
+                height:'50px',
+                border:'none',
+                backgroundColor:'green',
+                borderRadius:'10px',
+                color:'white',
+                cursor:'pointer',
+            }}>Click me</button>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<R18App />)
